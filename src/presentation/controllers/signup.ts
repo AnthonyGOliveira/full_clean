de9c62afcbb1f2201cf899802d8131c0601aef18
AnthonyGOliveira <anthony.glivers@gmail.com>
2024-09-30 21:52:21 -0300

    feat: ensure signUpController return message error if confirmationPassword is not provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,7 +4,12 @@ import { HttpRequest, HttpResponse } from "../protocols/http";
 
 export class SignUpController {
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const validateFields = ["name", "email", "password"];
+    const validateFields = [
+      "name",
+      "email",
+      "password",
+      "confirmationPassword",
+    ];
     for (const field of validateFields) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParam(field));
