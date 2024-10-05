d3cc75f1b48420201630109a6f5b5685eb70d199
AnthonyGOliveira <anthony.glivers@gmail.com>
2024-10-05 14:32:56 -0300

    test: adding internal server error handling

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,6 +1,8 @@
 import { InvalidParam } from "../errors/invalid-param-error";
 import { MissingParam } from "../errors/missing-param-error";
+import { ServerError } from "../errors/server-error";
 import { EmailValidator } from "../protocols/email-validator";
+import { Logger } from "../protocols/logger";
 import { SignUpController } from "./signup";
 
 const makeSut = () => {
@@ -9,11 +11,20 @@ const makeSut = () => {
       return true;
     }
   }
+
+  class LoggerStub implements Logger {
+    info():void {};
+    debug():void {};
+    warning():void {};
+    error():void {};
+  }
   const emailValidator = new EmailValidatorStub();
-  const sut = new SignUpController(emailValidator);
+  const logger = new LoggerStub();
+  const sut = new SignUpController(emailValidator, logger);
   return {
     sut,
     emailValidator,
+    logger,
   };
 };
 
@@ -87,5 +98,47 @@ describe("SignUp Controller", () => {
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new InvalidParam("email"));
     expect(emailValidatorSpy).toHaveBeenCalledWith(httpRequest.body.email);
+  });
+  test("Should email validator called with correct value", async () => {
+    const { sut, emailValidator } = makeSut();
+    const httpRequest = {
+      body: {
+        name: "any_name",
+        email: "any_email",
+        password: "any_password",
+        confirmationPassword: "any_password",
+      },
+    };
+    const emailValidatorSpy = jest
+      .spyOn(emailValidator, "isValid")
+      .mockReturnValueOnce(true);
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(200);
+    expect(httpResponse.body).toEqual({
+      message: "Signup is successfull",
+    });
+    expect(emailValidatorSpy).toHaveBeenCalledWith(httpRequest.body.email);
+  });
+  test("Should return 500 if internal server error occurred", async () => {
+    const { sut, emailValidator, logger } = makeSut();
+    const httpRequest = {
+      body: {
+        name: "any_name",
+        email: "any_email",
+        password: "any_password",
+        confirmationPassword: "any_password",
+      },
+    };
+    const emailValidatorSpy = jest
+      .spyOn(emailValidator, "isValid")
+      .mockImplementationOnce(() => {
+        throw new Error("Error");
+      });
+    const spyLoggerError = jest.spyOn(logger, "error");
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(500);
+    expect(httpResponse.body).toEqual(new ServerError());
+    expect(emailValidatorSpy).toHaveBeenCalledWith(httpRequest.body.email);
+    expect(spyLoggerError).toHaveBeenCalledWith(new Error('Error'));
   });
 });
