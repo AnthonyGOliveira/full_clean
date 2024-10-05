a68dda262dfd6afb298eb6b3a81956144f602b85
AnthonyGOliveira <anthony.glivers@gmail.com>
2024-10-05 14:32:47 -0300

    feat: adding internal server error handling

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,36 +1,44 @@
 import { InvalidParam } from "../errors/invalid-param-error";
 import { MissingParam } from "../errors/missing-param-error";
-import { badRequest } from "../helpers/http-helpers";
+import { badRequest, serverError } from "../helpers/http-helpers";
 import { Controller } from "../protocols/controller";
 import { EmailValidator } from "../protocols/email-validator";
 import { HttpRequest, HttpResponse } from "../protocols/http";
+import { Logger } from "../protocols/logger";
 
 export class SignUpController implements Controller {
   emailValidator: EmailValidator;
-  constructor(emailValidator: EmailValidator) {
+  logger: Logger;
+  constructor(emailValidator: EmailValidator, logger: Logger) {
     this.emailValidator = emailValidator;
+    this.logger = logger;
   }
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const validateFields = [
-      "name",
-      "email",
-      "password",
-      "confirmationPassword",
-    ];
-    for (const field of validateFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParam(field));
+    try {
+      const validateFields = [
+        "name",
+        "email",
+        "password",
+        "confirmationPassword",
+      ];
+      for (const field of validateFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParam(field));
+        }
       }
+      if (!this.emailValidator.isValid(httpRequest.body.email)) {
+        return badRequest(new InvalidParam("email"));
+      }
+      return {
+        statusCode: 200,
+        body: {
+          message: "Signup is successfull",
+        },
+      };
+    } catch (error) {
+      this.logger.error(error)
+      return serverError()
     }
-    if (!this.emailValidator.isValid(httpRequest.body.email)) {
-      return badRequest(new InvalidParam("email"));
-    }
-    return {
-      statusCode: 200,
-      body: {
-        message: "Signup is successfull",
-      },
-    };
   }
 }
