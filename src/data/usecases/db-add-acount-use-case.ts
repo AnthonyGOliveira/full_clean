42e21bc170ad87a0cc9044b3e4504480346538a9
AnthonyGOliveira <anthony.glivers@gmail.com>
2024-10-08 21:43:52 -0300

    feat: adding AddAcountRepository in DbAddAcountUseCase

--- src/data/usecases/db-add-acount-use-case.ts
+++ src/data/usecases/db-add-acount-use-case.ts
@@ -3,23 +3,23 @@ import {
   AddAcount,
   AddAcountUseCase,
 } from "../../domain/usecases/add-acount-use-case";
+import { AddAcountRepository } from "../protocols/add-acount-repository";
 import { Encrypter } from "../protocols/encrypter";
 
 export class DbAddAcountUseCase implements AddAcountUseCase {
   private readonly encrypter: Encrypter;
+  private readonly addAcountRepository: AddAcountRepository;
 
-  constructor(encrypter: Encrypter) {
+  constructor(encrypter: Encrypter, addAcountRepository: AddAcountRepository) {
     this.encrypter = encrypter;
+    this.addAcountRepository = addAcountRepository;
   }
 
   async execute(addAcount: AddAcount): Promise<AddAcountModel> {
     const hashedPassword = await this.encrypter.encrypt(addAcount.password);
-    const addAcountModel = {
-      id: "valid_id",
-      name: addAcount.name,
-      email: addAcount.email,
-      password: hashedPassword,
-    };
-    return new Promise((resolve) => resolve(addAcountModel));
+    const addAcountModel = await this.addAcountRepository.add(
+      Object.assign({}, addAcount, { password: hashedPassword })
+    );
+    return addAcountModel;
   }
 }
