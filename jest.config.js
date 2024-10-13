10d01b4c6a0f8c44fd8876af4b9fd05299afaf9c
AnthonyGOliveira <anthony.glivers@gmail.com>
2024-10-12 23:58:56 -0300

    chore: add ignore paths in jest.config

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,13 @@
 module.exports = {
-  roots: ['<rootDir>/src'], // Múltiplas pastas de testes
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'], // Ignorar pastas __tests__
-  coverageDirectory: 'coverage',
-  testEnvironment: 'node',
+  roots: ["<rootDir>/src"], // Múltiplas pastas de testes
+  collectCoverageFrom: ["<rootDir>/src/**/*.ts"], // Ignorar pastas __tests__
+  coverageDirectory: "coverage",
+  testEnvironment: "node",
   transform: {
-    '.+\\.ts$': 'ts-jest'
+    ".+\\.ts$": "ts-jest",
   },
+  coveragePathIgnorePatterns: [
+    "/node_modules/",
+    "<rootDir>/src/infra/db/mongodb/helpers/mongodb-mocking.ts", // Ignora o arquivo específico
+  ],
 };
