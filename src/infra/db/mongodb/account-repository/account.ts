c385a27875daf64f421a853e0777ff906b8bb316
AnthonyGOliveira <anthony.glivers@gmail.com>
2024-10-13 00:01:50 -0300

    feat: add account mongo repository

---
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,15 @@
+import { AddAcountModel } from "../../../../data/models/add-acount-model";
+import {
+  AddAcount,
+  AddAcountRepository,
+} from "../../../../data/protocols/add-acount-repository";
+import { mapperToAccontModel } from "../../../mapper/account-mapper";
+import { MongoDatabaseHelper } from "../helpers/mongodb-helper";
+
+export class AccountMongoRepository implements AddAcountRepository {
+  async add(addAcount: AddAcount): Promise<AddAcountModel> {
+    const accountCollection = MongoDatabaseHelper.getCollection("accounts");
+    const result = await accountCollection.insertOne(addAcount);
+    return mapperToAccontModel(addAcount, result.insertedId.toString());
+  }
+}
