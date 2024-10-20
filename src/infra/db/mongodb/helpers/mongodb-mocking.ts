d37fb63bf49b4027f7df7bfa8ae7f682eda3653d
AnthonyGOliveira <anthony.glivers@gmail.com>
2024-10-20 10:35:06 -0300

    refactor: refactor methods in MongoMock

--- src/infra/db/mongodb/helpers/mongodb-mocking.ts
+++ src/infra/db/mongodb/helpers/mongodb-mocking.ts
@@ -1,30 +1,41 @@
-// mongodb-mocking.ts
 import { randomUUID } from "crypto";
 
 export const MongoMock = () => {
   const db = {
-    collections: {
-      accounts: [],
-    },
+    collections: {},
   };
+  let isConnected: boolean = false;
 
   return {
-    connect: jest.fn().mockResolvedValue(true),
-    close: jest.fn().mockResolvedValue(true),
-    db: jest.fn().mockReturnValue({
-      collection: (name: string) => ({
-        insertOne: jest.fn().mockImplementation((data) => {
-          const id = randomUUID(); // Gerando uma ID aleatória
-          db.collections[name].push({ ...data, _id: id });
-          return { insertedId: id };
-        }),
-        findOne: jest.fn().mockImplementation((query) => {
-          return db.collections[name].find((item) => item._id === query._id);
-        }),
-        deleteMany: jest.fn().mockImplementation(() => {
-          db.collections[name] = [];
-        }),
-      }),
+    connect: jest.fn().mockImplementation(() => {
+      isConnected = true;
+      return true;
     }),
+    close: jest.fn().mockImplementation(() => {
+      isConnected = false;
+      return true;
+    }),
+    db: jest.fn().mockReturnValue({
+      collection: (name: string) => {
+        if (!db.collections[name]) {
+          db.collections[name] = []; // Cria a coleção se não existir
+        }
+        return {
+          collectionName: name,
+          insertOne: jest.fn().mockImplementation((data) => {
+            const id = randomUUID(); // Gera uma ID aleatória
+            db.collections[name].push({ ...data, _id: id });
+            return { insertedId: id };
+          }),
+          findOne: jest.fn().mockImplementation((query) => {
+            return db.collections[name].find((item) => item._id === query._id);
+          }),
+          deleteMany: jest.fn().mockImplementation(() => {
+            db.collections[name] = [];
+          }),
+        };
+      },
+    }),
+    isConnected: jest.fn().mockReturnValue(isConnected),
   };
 };
