80afbd82ec58e95b2eb9980ec0e7b234f2f5417e
AnthonyGOliveira <anthony.glivers@gmail.com>
2024-10-19 21:42:18 -0300

    feat: adding Logger interface

--- src/presentation/protocols/logger.ts
+++ src/presentation/protocols/logger.ts
@@ -1,6 +1,6 @@
 export interface Logger {
-  info: (message: string) => void;
-  error: (message: string) => void;
-  warning: (message: string) => void;
-  debug: (message: string) => void;
+  info(message: string, meta: any): void;
+  error(error: Error, meta?: any): void;
+  warning(message: string, meta: any): void;
+  debug(message: string, meta: any): void;
 }
