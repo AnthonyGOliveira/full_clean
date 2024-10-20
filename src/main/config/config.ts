9bd885dc233138d6e7f45718706373815b683133
AnthonyGOliveira <anthony.glivers@gmail.com>
2024-10-19 21:43:35 -0300

    chore: adding config file

--- src/main/config/config.ts
+++ src/main/config/config.ts
@@ -2,4 +2,12 @@ export const config = {
   server: {
     port: process.env.SERVER_PORT || 3000,
   },
+  database: {
+    url: process.env.MONGO_URL || "mongodb://root:root@localhost:27017",
+    name: process.env.MONGO_DATABASE_NAME || "clean-node-api",
+  },
+  logger: {
+    filenamePath: process.env.LOGGER_FILENAME_PATH || "logs/app.log",
+    level: process.env.LOGGER_LEVEL || "info",
+  },
 };
