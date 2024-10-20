4738d5990c0f00c1311c9b7eb7af24c5c6eed0d2
AnthonyGOliveira <anthony.glivers@gmail.com>
2024-10-19 21:44:32 -0300

    feat: adding action to connect database in server file

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,6 +1,13 @@
 import app from "./config/app";
 import { config } from "./config/config";
+import { MongoDatabaseHelper } from "../infra/db/mongodb/helpers/mongodb-helper";
 
-app.listen(config.server.port, () => {
-  console.log(`Server is running on port: ${config.server.port}`);
-});
+const databaseUrl = `${config.database.url}/${config.database.name}?authSource=admin`;
+
+MongoDatabaseHelper.connect(databaseUrl)
+  .then(() => {
+    app.listen(config.server.port, () => {
+      console.log(`Server is running on port: ${config.server.port}`);
+    });
+  })
+  .catch(console.error);
