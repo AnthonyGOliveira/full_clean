214b35dabde79eb135936f081ffa45f7f0f08beb
AnthonyGOliveira <anthony.glivers@gmail.com>
2024-10-26 17:51:53 -0300

    test: tests of LogErrorRepositoryDecorator

---
+++ src/infra/decorators/log-error-repository.spec.ts
@@ -0,0 +1,88 @@
+import { LogErrorRepositoryDecorator } from "./log-error-repository";
+import { MongoDatabaseHelper } from "../db/mongodb/helpers/mongodb-helper";
+import { Collection, Document } from "mongodb";
+
+let insertOneMock = jest.fn();
+
+jest.mock("../db/mongodb/helpers/mongodb-helper", () => {
+  return {
+    MongoDatabaseHelper: {
+      connect: jest.fn(),
+      disconnect: jest.fn(),
+      getCollection: jest.fn(),
+    },
+  };
+});
+
+interface ILogError {
+  error: Error;
+  meta: any;
+}
+
+interface Logger {
+  info(message: string, meta: any): void;
+  error(error: Error, meta?: any): void;
+  warning(message: string, meta: any): void;
+  debug(message: string, meta: any): void;
+}
+
+const makeLoggerAdapter = () => {
+  class LoggerAdapter implements Logger {
+    info(message: string, meta: any): void {
+      jest.fn();
+    }
+    error(error: Error, meta?: any): void {
+      jest.fn();
+    }
+    warning(message: string, meta: any): void {
+      jest.fn();
+    }
+    debug(message: string, meta: any): void {
+      jest.fn();
+    }
+  }
+
+  return new LoggerAdapter();
+};
+
+const makeSut = () => {
+  const logger = makeLoggerAdapter();
+  const decorator = new LogErrorRepositoryDecorator(logger);
+  return {
+    sut: decorator,
+    logger,
+  };
+};
+
+describe("LogErrorRepositoryDecorator", () => {
+  beforeAll(() => {
+    jest
+      .spyOn(MongoDatabaseHelper, "getCollection")
+      .mockImplementation((): Promise<Collection<Document>> => {
+        return new Promise(
+          (resolve) =>
+            resolve({
+              insertOne: insertOneMock,
+            } as unknown as Collection<Document>) // Conversão para simular uma Collection
+        );
+      });
+  });
+  test("Should insert error log in database", async () => {
+    const { sut, logger } = makeSut();
+    const decoratorSpy = jest.spyOn(sut, "error");
+    const loggerSpy = jest.spyOn(logger, "error");
+    const error = new Error("Error");
+    const meta = { any: "any_value" };
+    const errorLog: ILogError = {
+      error: error,
+      meta: meta,
+    };
+    await sut.error(error, meta);
+    expect(decoratorSpy).toHaveBeenCalledWith(error, meta);
+    expect(loggerSpy).toHaveBeenCalledWith(error, meta);
+    expect(MongoDatabaseHelper.getCollection).toHaveBeenCalledWith(
+      "log-error"
+    );
+    expect(insertOneMock).toHaveBeenCalledWith(errorLog);
+  });
+});
