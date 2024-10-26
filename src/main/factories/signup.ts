409a8c5031aa537a8338b6066a42e353880bf54c
AnthonyGOliveira <anthony.glivers@gmail.com>
2024-10-26 17:47:09 -0300

    feat: adding Log controller decorator in SignUpControllerFactory

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -1,16 +1,24 @@
 import { DbAddAcountUseCase } from "../../data/usecases/db-add-acount-use-case";
 import { AccountMongoRepository } from "../../infra/db/mongodb/account-repository/account";
 import { BCryptAdapter } from "../../infra/encryption/bcrypt-adapter";
+import { LogControllerDecorator } from "../../presentation/controllers/decorators/log-controller";
 import { SignUpController } from "../../presentation/controllers/signup";
 import { Controller } from "../../presentation/protocols/controller";
 import { EmailValidatorAdapter } from "../../utils/email-validator-adapter";
-import { LoggerAdapter } from "../../utils/logger-adapter";
+import LoggerFactory from "./logger";
 
 export default (): Controller => {
-    const emailValidator = new EmailValidatorAdapter()
-    const logger = new LoggerAdapter()
-    const encrypter = new BCryptAdapter()
-    const accountMongoRepository = new AccountMongoRepository()
-    const addAcountUseCase = new DbAddAcountUseCase(encrypter, accountMongoRepository)
-    return new SignUpController(emailValidator, logger, addAcountUseCase)
-}
+  const emailValidator = new EmailValidatorAdapter();
+  const encrypter = new BCryptAdapter();
+  const accountMongoRepository = new AccountMongoRepository();
+  const addAcountUseCase = new DbAddAcountUseCase(
+    encrypter,
+    accountMongoRepository
+  );
+  const signUpController = new SignUpController(
+    emailValidator,
+    addAcountUseCase
+  );
+  const logger = LoggerFactory();
+  return new LogControllerDecorator(signUpController, logger);
+};
