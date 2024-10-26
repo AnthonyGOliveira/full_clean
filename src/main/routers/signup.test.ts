8219fb8de13c49c2122c76c73e1e20713481de29
AnthonyGOliveira <anthony.glivers@gmail.com>
2024-10-26 17:52:55 -0300

    refactor: format Signup Router test

--- src/main/routers/signup.test.ts
+++ src/main/routers/signup.test.ts
@@ -14,7 +14,9 @@ describe("Signup Router", () => {
     await MongoDatabaseHelper.connect(process.env.MONGO_URL);
   });
   afterEach(async () => {
-    const accountCollection = await MongoDatabaseHelper.getCollection("accounts");
+    const accountCollection = await MongoDatabaseHelper.getCollection(
+      "accounts"
+    );
     await accountCollection.deleteMany({});
   });
   afterAll(async () => {
