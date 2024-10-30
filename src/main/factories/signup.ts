f20dca2ce0ed6784e05ccde09742e11d74330dd2
AnthonyGOliveira <anthony.glivers@gmail.com>
2024-10-30 20:29:40 -0300

    refactor: adding signup controller in factory

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -2,7 +2,7 @@ import { DbAddAcountUseCase } from "../../data/usecases/db-add-acount-use-case";
 import { AccountMongoRepository } from "../../infra/db/mongodb/account-repository/account";
 import { BCryptAdapter } from "../../infra/encryption/bcrypt-adapter";
 import { LogControllerDecorator } from "../../presentation/controllers/decorators/log-controller";
-import { SignUpController } from "../../presentation/controllers/signup";
+import { SignUpController } from "../../presentation/controllers/signup/signup";
 import { Controller } from "../../presentation/protocols/controller";
 import { EmailValidatorAdapter } from "../../utils/email-validator-adapter";
 import LoggerFactory from "./logger";
