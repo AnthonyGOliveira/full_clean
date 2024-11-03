ae19004b566267853862676059cd17f3f2d06121
AnthonyGOliveira <anthony.glivers@gmail.com>
2024-11-03 09:37:40 -0300

    feat: should return 401 if AuthenticationUseCase not return AuthenticationResponse

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,19 +1,25 @@
 import { MissingParam } from "../../errors/missing-param-error";
-import { badRequest, serverError, ok } from "../../helpers/http-helpers";
+import {
+  badRequest,
+  serverError,
+  ok,
+  unauthorizedRequest,
+} from "../../helpers/http-helpers";
 import { Controller } from "../../protocols/controller";
 import { HttpRequest, HttpResponse } from "../../protocols/http";
-import { FindAccountUseCase } from "../../../domain/usecases/find-account-use-case";
+import { AuthenticationUseCase } from "../../../domain/usecases/authentication-use-case";
 import { EmailValidator } from "../../protocols/email-validator";
 import { InvalidParam } from "../../errors/invalid-param-error";
+import { Unauthorized } from "../../errors/unauthorized-error";
 
 export class LoginController implements Controller {
-  private readonly findAccountUseCase: FindAccountUseCase;
+  private readonly authenticationUseCase: AuthenticationUseCase;
   private readonly emailValidator: EmailValidator;
   constructor(
-    findAccountUseCase: FindAccountUseCase,
+    authenticationUseCase: AuthenticationUseCase,
     emailValidator: EmailValidator
   ) {
-    this.findAccountUseCase = findAccountUseCase;
+    this.authenticationUseCase = authenticationUseCase;
     this.emailValidator = emailValidator;
   }
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
@@ -29,8 +35,13 @@ export class LoginController implements Controller {
       if (!this.emailValidator.isValid(email)) {
         return badRequest(new InvalidParam("email or password"));
       }
-
-      const result = await this.findAccountUseCase.execute({ email, password });
+      const result = await this.authenticationUseCase.execute({
+        email,
+        password,
+      });
+      if (!result) {
+        return unauthorizedRequest(new Unauthorized());
+      }
       return ok(result);
     } catch (error) {
       return serverError(error);
