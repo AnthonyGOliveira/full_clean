65eebe2d479a5de233bdd1a0443ca43510b62127
AnthonyGOliveira <anthony.glivers@gmail.com>
2024-11-06 21:27:03 -0300

    feat: adding CompareFieldsValidation in ValidationComposite

--- src/main/factories/validation-composite/signup-validation-composite.ts
+++ src/main/factories/validation-composite/signup-validation-composite.ts
@@ -1,3 +1,4 @@
+import { CompareFieldsValidation } from "../../../presentation/helpers/validators/compare-fields-validation/compare-fields-validation";
 import { RequiredFieldValidation } from "../../../presentation/helpers/validators/required-field-validation/required-field-validation";
 import { Validation } from "../../../presentation/helpers/validators/validation";
 import { ValidationComposite } from "../../../presentation/helpers/validators/validation-composite/validation-composite";
@@ -8,5 +9,6 @@ export default (): Validation => {
     new RequiredFieldValidation("email"),
     new RequiredFieldValidation("password"),
     new RequiredFieldValidation("confirmationPassword"),
+    new CompareFieldsValidation("password", "confirmationPassword"),
   ]);
 };
