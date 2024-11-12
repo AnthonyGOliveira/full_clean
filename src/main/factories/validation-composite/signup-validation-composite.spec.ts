222938a9b00b91b13caa19cdf1e58ab4cb4fe654
AnthonyGOliveira <anthony.glivers@gmail.com>
2024-11-12 19:23:45 -0300

    test: adding EmailValidation in ValidationComposite

--- src/main/factories/validation-composite/signup-validation-composite.spec.ts
+++ src/main/factories/validation-composite/signup-validation-composite.spec.ts
@@ -1,6 +1,8 @@
 import { CompareFieldsValidation } from "../../../presentation/helpers/validators/compare-fields-validation/compare-fields-validation";
+import { EmailValidation } from "../../../presentation/helpers/validators/email-validation/email-validation";
 import { RequiredFieldValidation } from "../../../presentation/helpers/validators/required-field-validation/required-field-validation";
 import { ValidationComposite } from "../../../presentation/helpers/validators/validation-composite/validation-composite";
+import { EmailValidatorAdapter } from "../../../utils/email-validator-adapter";
 import SignupValidationCompositeFactory from "./signup-validation-composite";
 
 jest.mock(
@@ -23,6 +25,7 @@ describe("ValidationComposite", () => {
       new RequiredFieldValidation("password"),
       new RequiredFieldValidation("confirmationPassword"),
       new CompareFieldsValidation("password", "confirmationPassword"),
+      new EmailValidation(new EmailValidatorAdapter()),
     ];
     expect(ValidationComposite).toHaveBeenCalledWith(validations);
   });
