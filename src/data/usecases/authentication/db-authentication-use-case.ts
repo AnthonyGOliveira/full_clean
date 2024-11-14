e0995b4618947b4e3d761ce4d89ad1a121e516ea
AnthonyGOliveira <anthony.glivers@gmail.com>
2024-11-14 17:57:47 -0300

    feat: should DbAuthenticationUseCase call repository

--- src/data/usecases/authentication/db-authentication-use-case.ts
+++ src/data/usecases/authentication/db-authentication-use-case.ts
@@ -3,9 +3,16 @@ import {
   AuthResponse,
   LoginModel,
 } from "../../../domain/usecases/authentication-use-case";
+import { FindAccountByEmailRepository } from "../../protocols/find-account-repository";
 
 export class DbAuthenticationUseCase implements AuthenticationUseCase {
-  execute(addAcount: LoginModel): Promise<AuthResponse | null> {
+  private readonly findAccountByEmailRepository: FindAccountByEmailRepository;
+
+  constructor(findAccountByEmailRepository: FindAccountByEmailRepository) {
+    this.findAccountByEmailRepository = findAccountByEmailRepository;
+  }
+  async execute(loginAccount: LoginModel): Promise<AuthResponse | null> {
+    await this.findAccountByEmailRepository.find(loginAccount.email);
     return null;
   }
 }
