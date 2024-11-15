94bfd20c5837b9f95bf72e7357175750abfe7ddb
AnthonyGOliveira <anthony.glivers@gmail.com>
2024-11-15 12:05:19 -0300

    test: should DbAuthenticationUseCase call PasswordValidator

--- src/data/usecases/authentication/db-authentication-use-case.spec.ts
+++ src/data/usecases/authentication/db-authentication-use-case.spec.ts
@@ -1,31 +1,54 @@
 import { FindAcountModel } from "../../models/find-account-model";
 import { FindAccountByEmailRepository } from "../../protocols/find-account-repository";
+import { PasswordValidator } from "../../protocols/password-validator";
 import { DbAuthenticationUseCase } from "./db-authentication-use-case";
 
 interface TypeSut {
   sut: DbAuthenticationUseCase;
   repository: FindAccountByEmailRepository;
+  passwordValidator: PasswordValidator;
 }
 
 const makeFindAccountByEmailRepositoryStub =
   (): FindAccountByEmailRepository => {
+    const accountModel: FindAcountModel = {
+        id: "123",
+        name: "any_name",
+        email: "any@email.com",
+        password: "any_hash"
+    }
     class FindAccountByEmailRepositoryStub
       implements FindAccountByEmailRepository
     {
       async find(email: string): Promise<FindAcountModel> {
-        return new Promise((resolve) => resolve(null));
+        return new Promise((resolve) => resolve(accountModel));
       }
     }
 
     return new FindAccountByEmailRepositoryStub();
   };
 
+const makePasswordValidatorStub = (): PasswordValidator => {
+  class PasswordValidatorStub implements PasswordValidator {
+    async compare(password: string, hash: string): Promise<boolean> {
+      return new Promise((resolve) => resolve(true));
+    }
+  }
+
+  return new PasswordValidatorStub();
+};
+
 const makeSut = (): TypeSut => {
   const findAccountRepository = makeFindAccountByEmailRepositoryStub();
-  const sut = new DbAuthenticationUseCase(findAccountRepository);
+  const passwordValidatorStub = makePasswordValidatorStub();
+  const sut = new DbAuthenticationUseCase(
+    findAccountRepository,
+    passwordValidatorStub
+  );
   return {
     sut,
     repository: findAccountRepository,
+    passwordValidator: passwordValidatorStub,
   };
 };
 
@@ -53,5 +76,16 @@ describe("DbAuthenticationUseCase", () => {
     };
     const result = await sut.execute(login);
     expect(result).toEqual(null);
+  });
+  test("should DbAuthenticationUseCase call PasswordValidator", async () => {
+    const { sut, passwordValidator } = makeSut();
+    const spyRepository = jest.spyOn(passwordValidator, "compare");
+    const login = {
+      email: "any@email.com",
+      password: "any_password",
+    };
+    const expectHash = "any_hash";
+    await sut.execute(login);
+    expect(spyRepository).toHaveBeenCalledWith(login.password, expectHash);
   });
 });
