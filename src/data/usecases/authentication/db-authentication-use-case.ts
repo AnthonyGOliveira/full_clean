181da2058e8a456c3a5ea9bf3a837b950d0ee5e7
AnthonyGOliveira <anthony.glivers@gmail.com>
2024-11-15 06:21:31 -0300

    feat: should DbAuthenticationUseCase call PasswordValidator

--- src/data/usecases/authentication/db-authentication-use-case.ts
+++ src/data/usecases/authentication/db-authentication-use-case.ts
@@ -4,15 +4,30 @@ import {
   LoginModel,
 } from "../../../domain/usecases/authentication-use-case";
 import { FindAccountByEmailRepository } from "../../protocols/find-account-repository";
+import { PasswordValidator } from "../../protocols/password-validator";
 
 export class DbAuthenticationUseCase implements AuthenticationUseCase {
   private readonly findAccountByEmailRepository: FindAccountByEmailRepository;
+  private readonly passwordValidator: PasswordValidator;
 
-  constructor(findAccountByEmailRepository: FindAccountByEmailRepository) {
+  constructor(
+    findAccountByEmailRepository: FindAccountByEmailRepository,
+    passwordValidator: PasswordValidator
+  ) {
     this.findAccountByEmailRepository = findAccountByEmailRepository;
+    this.passwordValidator = passwordValidator;
   }
   async execute(loginAccount: LoginModel): Promise<AuthResponse | null> {
-    await this.findAccountByEmailRepository.find(loginAccount.email);
-    return null;
+    const account = await this.findAccountByEmailRepository.find(
+      loginAccount.email
+    );
+    if (!account) {
+      return null;
+    }
+
+    await this.passwordValidator.compare(
+      loginAccount.password,
+      account.password
+    );
   }
 }
