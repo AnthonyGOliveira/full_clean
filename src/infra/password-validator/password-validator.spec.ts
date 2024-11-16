08fbdb96317c545112e9d75119caba5a56008ebc
AnthonyGOliveira <anthony.glivers@gmail.com>
2024-11-16 10:32:23 -0300

    test: should throw an error if PasswordValidator generate error

--- src/infra/password-validator/password-validator.spec.ts
+++ src/infra/password-validator/password-validator.spec.ts
@@ -31,4 +31,16 @@ describe("PasswordValidator", () => {
     expect(spyBcrypt).toHaveBeenCalledWith(password, hashed_password);
     expect(result).toBe(false);
   });
+  test("should throw an error if PasswordValidator generate error", async () => {
+    const { sut } = makeSut();
+    const error = new Error();
+    const spyBcrypt = jest.spyOn(bcrypt, "compare").mockImplementation(() => {
+      throw error;
+    });
+    const password = "any_password";
+    const hashed_password = "hashed_password";
+    const promise = sut.compare(password, hashed_password);
+    expect(spyBcrypt).toHaveBeenCalledWith(password, hashed_password);
+    await expect(promise).rejects.toThrow();
+  });
 });
