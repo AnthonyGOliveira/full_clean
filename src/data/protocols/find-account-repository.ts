2e19d92506e2f3d70722a759db519cb0a8e60c9d
AnthonyGOliveira <anthony.glivers@gmail.com>
2024-11-16 09:41:53 -0300

    feat: adding FindAccountRepo interface

--- src/data/protocols/find-account-repository.ts
+++ src/data/protocols/find-account-repository.ts
@@ -1,5 +1,12 @@
 import { FindAcountModel } from "../models/find-account-model";
 
+export interface FindAccountRepo {
+  _id: string;
+  name: string;
+  email: string;
+  password: string;
+}
+
 export interface FindAccountByEmailRepository {
   find(email: string): Promise<FindAcountModel>;
 }
