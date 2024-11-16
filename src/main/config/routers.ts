32799a4083eb5de18486889ec0b3f8d55f6ee1e6
AnthonyGOliveira <anthony.glivers@gmail.com>
2024-11-16 19:53:22 -0300

    feat: adding Login Router in router

--- src/main/config/routers.ts
+++ src/main/config/routers.ts
@@ -1,7 +1,9 @@
 import { Express, Router } from "express";
-import SignUpRouter from "../routers/signup";
+import SignUpRouter from "../routers/signup/signup";
+import LoginRouter from "../routers/login/login";
 export default (app: Express) => {
   const router = Router();
   app.use("/api", router);
   SignUpRouter(router);
+  LoginRouter(router);
 };
