f8aeb1839ec8bcf53eb1eda96f33b4be608088af
AnthonyGOliveira <anthony.glivers@gmail.com>
2024-11-16 09:43:01 -0300

    feat: adding mapperToFindAccountModel

---
+++ src/infra/mapper/find-account-mapper.ts
@@ -0,0 +1,13 @@
+import { FindAcountModel } from "../../data/models/find-account-model";
+import { FindAccountRepo } from "../../data/protocols/find-account-repository";
+
+export const mapperToFindAccountModel = (
+  findAccount: FindAccountRepo
+): FindAcountModel => {
+  return {
+    id: findAccount._id,
+    name: findAccount.name,
+    email: findAccount.email,
+    password: findAccount.password,
+  };
+};
