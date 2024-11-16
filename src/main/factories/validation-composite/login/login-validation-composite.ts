b2fdb9299f191ad68ce04c2acb72d9acf592d2cc
AnthonyGOliveira <anthony.glivers@gmail.com>
2024-11-16 19:50:30 -0300

    refactor: should create LoginValidationComposite instance correct validations

--- src/main/factories/validation-composite/login/login-validation-composite.ts
+++ src/main/factories/validation-composite/login/login-validation-composite.ts
@@ -1,6 +1,14 @@
+import { EmailValidation } from "../../../../presentation/helpers/validators/email-validation/email-validation";
+import { RequiredFieldValidation } from "../../../../presentation/helpers/validators/required-field-validation/required-field-validation";
 import { Validation } from "../../../../presentation/helpers/validators/validation";
 import { ValidationComposite } from "../../../../presentation/helpers/validators/validation-composite/validation-composite";
+import { EmailValidatorAdapter } from "../../../../utils/email-validator-adapter";
 
 export default (): Validation => {
-  return new ValidationComposite([]);
+  const emailValidator = new EmailValidatorAdapter();
+  return new ValidationComposite([
+    new RequiredFieldValidation("email"),
+    new RequiredFieldValidation("password"),
+    new EmailValidation(emailValidator),
+  ]);
 };
