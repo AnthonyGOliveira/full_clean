118e9169084534fcb701ce1ea0f6d464bae85634
AnthonyGOliveira <anthony.glivers@gmail.com>
2024-11-16 09:41:37 -0300

    feat: adding FindAcountModel interface

--- src/data/models/find-account-model.ts
+++ src/data/models/find-account-model.ts
@@ -1,7 +1,6 @@
 export interface FindAcountModel {
-    id: string;
-    name: string;
-    email: string;
-    password: string;
-  }
-  
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+}
