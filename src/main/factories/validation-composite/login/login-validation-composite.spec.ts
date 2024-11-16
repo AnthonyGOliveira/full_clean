dc7cf11ba1a99c5da98978d8f2213f25f74d82ae
AnthonyGOliveira <anthony.glivers@gmail.com>
2024-11-16 19:50:41 -0300

    test: should create LoginValidationComposite instance correct validations

--- src/main/factories/validation-composite/login/login-validation-composite.spec.ts
+++ src/main/factories/validation-composite/login/login-validation-composite.spec.ts
@@ -1,4 +1,7 @@
+import { EmailValidation } from "../../../../presentation/helpers/validators/email-validation/email-validation";
+import { RequiredFieldValidation } from "../../../../presentation/helpers/validators/required-field-validation/required-field-validation";
 import { ValidationComposite } from "../../../../presentation/helpers/validators/validation-composite/validation-composite";
+import { EmailValidatorAdapter } from "../../../../utils/email-validator-adapter";
 import LoginValidationCompositeFactory from "./login-validation-composite";
 
 jest.mock(
@@ -15,7 +18,11 @@ const makeSut = () => {
 describe("LoginValidationComposite", () => {
   test("should create LoginValidationComposite instance correct validations", () => {
     const { sut } = makeSut();
-    const validations = [];
+    const validations = [
+      new RequiredFieldValidation("email"),
+      new RequiredFieldValidation("password"),
+      new EmailValidation(new EmailValidatorAdapter()),
+    ];
     expect(ValidationComposite).toHaveBeenCalledWith(validations);
   });
 });
