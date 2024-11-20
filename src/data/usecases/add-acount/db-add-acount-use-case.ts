da8e097b45c3968f60ad6fdd1132e557991b9c5c
AnthonyGOliveira <anthony.glivers@gmail.com>
2024-11-20 18:06:20 -0300

    feat: should throws error in DbAddAccountUseCase call and email already exists

--- src/data/usecases/add-acount/db-add-acount-use-case.ts
+++ src/data/usecases/add-acount/db-add-acount-use-case.ts
@@ -5,17 +5,28 @@ import {
 } from "../../../domain/usecases/add-acount-use-case";
 import { AddAcountRepository } from "../../protocols/add-acount-repository";
 import { Encrypter } from "../../protocols/encrypter";
+import { FindAccountByEmailRepository } from "../../protocols/find-account-repository";
 
 export class DbAddAcountUseCase implements AddAcountUseCase {
   private readonly encrypter: Encrypter;
   private readonly addAcountRepository: AddAcountRepository;
+  private readonly findAccountByEmailRepository: FindAccountByEmailRepository;
 
-  constructor(encrypter: Encrypter, addAcountRepository: AddAcountRepository) {
+  constructor(
+    encrypter: Encrypter,
+    addAcountRepository: AddAcountRepository,
+    findAccountByEmailRepository: FindAccountByEmailRepository
+  ) {
     this.encrypter = encrypter;
     this.addAcountRepository = addAcountRepository;
+    this.findAccountByEmailRepository = findAccountByEmailRepository;
   }
 
   async execute(addAcount: AddAcount): Promise<AddAcountModel> {
+    const emailExists = await this.findAccountByEmailRepository.find(addAcount.email);
+    if (emailExists) {
+      throw new Error("Email already registered");
+    }
     const hashedPassword = await this.encrypter.encrypt(addAcount.password);
     const addAcountModel = await this.addAcountRepository.add(
       Object.assign({}, addAcount, { password: hashedPassword })
