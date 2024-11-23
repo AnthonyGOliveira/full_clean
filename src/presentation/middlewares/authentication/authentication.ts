348bf7584973face7f604ec0b79ecb3f6e0c9a1b
AnthonyGOliveira <anthony.glivers@gmail.com>
2024-11-23 17:14:41 -0300

    feat: should return 500 if internal server error occurred in use case

--- src/presentation/middlewares/authentication/authentication.ts
+++ src/presentation/middlewares/authentication/authentication.ts
@@ -1,6 +1,6 @@
 import { AuthenticateTokenUseCase } from "../../../domain/usecases/authenticate-token-use -case";
 import { ForbiddenError } from "../../errors/forbidden-error";
-import { forbidden, ok } from "../../helpers/http-helpers";
+import { forbidden, ok, serverError } from "../../helpers/http-helpers";
 import { HttpRequest, HttpResponse } from "../../protocols/http";
 import { Middleware } from "../../protocols/middleware";
 
@@ -8,18 +8,18 @@ export class AuthenticationMiddleware implements Middleware {
   constructor(
     private readonly authenticateTokenUseCase: AuthenticateTokenUseCase
   ) {}
-  handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const { headers } = httpRequest;
       const token = this.getTokenFromHeaders(headers);
       const tokenResponse = this.authenticateTokenUseCase.execute(token);
       if (!tokenResponse) {
-        return new Promise((resolve) =>
-          resolve(forbidden(new ForbiddenError()))
-        );
+        return forbidden(new ForbiddenError());
       }
-      return new Promise((resolve) => resolve(ok({})));
-    } catch (error) {}
+      return ok(tokenResponse);
+    } catch (error) {
+      return serverError(error);
+    }
   }
   private getTokenFromHeaders(headers: any): string {
     return headers.Authorization.split(" ")[1];
