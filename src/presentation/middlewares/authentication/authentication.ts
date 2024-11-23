acb30a8e69aab7460b54361fce33fa0bbb658502
AnthonyGOliveira <anthony.glivers@gmail.com>
2024-11-23 17:04:47 -0300

    feat: should return 200 forbidden if user is authorized

--- src/presentation/middlewares/authentication/authentication.ts
+++ src/presentation/middlewares/authentication/authentication.ts
@@ -1,12 +1,27 @@
+import { AuthenticateTokenUseCase } from "../../../domain/usecases/authenticate-token-use -case";
 import { ForbiddenError } from "../../errors/forbidden-error";
-import { forbidden } from "../../helpers/http-helpers";
+import { forbidden, ok } from "../../helpers/http-helpers";
 import { HttpRequest, HttpResponse } from "../../protocols/http";
 import { Middleware } from "../../protocols/middleware";
 
 export class AuthenticationMiddleware implements Middleware {
+  constructor(
+    private readonly authenticateTokenUseCase: AuthenticateTokenUseCase
+  ) {}
   handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    return new Promise((resolve, reject) =>
-      resolve(forbidden(new ForbiddenError()))
-    );
+    try {
+      const { headers } = httpRequest;
+      const token = this.getTokenFromHeaders(headers);
+      const tokenResponse = this.authenticateTokenUseCase.execute(token);
+      if (!tokenResponse) {
+        return new Promise((resolve) =>
+          resolve(forbidden(new ForbiddenError()))
+        );
+      }
+      return new Promise((resolve) => resolve(ok({})));
+    } catch (error) {}
+  }
+  private getTokenFromHeaders(headers: any): string {
+    return headers.Authorization.split(" ")[1];
   }
 }
