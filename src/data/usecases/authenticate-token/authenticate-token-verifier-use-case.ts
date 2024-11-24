edd16402a18597f594a89128883fc041669ea35f
AnthonyGOliveira <anthony.glivers@gmail.com>
2024-11-24 09:49:47 -0300

    feat: should return null verifier dependency throw an error

--- src/data/usecases/authenticate-token/authenticate-token-verifier-use-case.ts
+++ src/data/usecases/authenticate-token/authenticate-token-verifier-use-case.ts
@@ -2,11 +2,26 @@ import {
   AuthenticateTokenResponse,
   AuthenticateTokenUseCase,
 } from "../../../domain/usecases/authenticate-token-use -case";
+import { AuthenticateTokenResponseMapper } from "../../mapper/authenticate-token-response-mapper";
+import { Logger } from "../../protocols/logger";
+import { TokenVerifier } from "../../protocols/token-verifier";
 
 export class AuthenticateTokenVerifierUseCase
   implements AuthenticateTokenUseCase
 {
+  constructor(
+    private readonly tokenVerifier: TokenVerifier,
+    private readonly logger: Logger
+  ) {}
   execute(token: string): AuthenticateTokenResponse | null {
-    return null;
+    try {
+      const tokenResponse = this.tokenVerifier.verify(token);
+      return AuthenticateTokenResponseMapper(tokenResponse);
+    } catch (error) {
+      this.logger.debug("An error occurred in the token verification process", {
+        error,
+      });
+      return null;
+    }
   }
 }
