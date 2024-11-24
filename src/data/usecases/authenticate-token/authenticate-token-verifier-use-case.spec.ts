900663546449554f9eb8fe2fce02cce8f1666330
AnthonyGOliveira <anthony.glivers@gmail.com>
2024-11-24 09:49:58 -0300

    test: should return null verifier dependency throw an error

--- src/data/usecases/authenticate-token/authenticate-token-verifier-use-case.spec.ts
+++ src/data/usecases/authenticate-token/authenticate-token-verifier-use-case.spec.ts
@@ -1,17 +1,77 @@
 import { AuthenticateTokenVerifierUseCase } from "./authenticate-token-verifier-use-case";
+import { Logger } from "../../protocols/logger";
+import {
+  TokenVerifier,
+  TokenVerifierResponse,
+} from "../../protocols/token-verifier";
 
-const makeSut = () => {
-  const sut = new AuthenticateTokenVerifierUseCase();
+type SutTypes = {
+  sut: AuthenticateTokenVerifierUseCase;
+  verifier: TokenVerifier;
+  logger: Logger;
+};
+
+const makeTokenVerifierStub = () => {
+  class TokenVerifierStub implements TokenVerifier {
+    verify(token: string): TokenVerifierResponse {
+      return {
+        id: "any_id",
+        email: "any@mail.com",
+        role: "any_role",
+      };
+    }
+  }
+
+  return new TokenVerifierStub();
+};
+
+const makeLoggerStub = () => {
+  class LoggerStub implements Logger {
+    debug(message: string, meta: any): void {
+      return;
+    }
+  }
+  return new LoggerStub();
+};
+
+const makeSut = (): SutTypes => {
+  const verifier = makeTokenVerifierStub();
+  const logger = makeLoggerStub();
+  const sut = new AuthenticateTokenVerifierUseCase(verifier, logger);
   return {
     sut,
+    verifier,
+    logger,
   };
 };
 
 describe("AuthenticateTokenVerifierUseCase", () => {
   test("should return null if token is not valid", () => {
-    const { sut } = makeSut();
+    const { sut, verifier } = makeSut();
     const token = "any_token";
+    const error = new Error("any_error");
+    jest.spyOn(verifier, "verify").mockImplementation(() => {
+      throw error;
+    });
     const result = sut.execute(token);
+    expect(result).toBeNull();
+  });
+  test("should return null verifier dependency throw an error", () => {
+    const { sut, verifier, logger } = makeSut();
+    const token = "any_token";
+    const error = new Error("any_error");
+    const verifierSpy = jest
+      .spyOn(verifier, "verify")
+      .mockImplementation(() => {
+        throw error;
+      });
+    const loggerSpy = jest.spyOn(logger, "debug");
+    const result = sut.execute(token);
+    expect(verifierSpy).toHaveBeenCalledWith(token);
+    expect(loggerSpy).toHaveBeenCalledWith(
+      "An error occurred in the token verification process",
+      { error }
+    );
     expect(result).toBeNull();
   });
 });
