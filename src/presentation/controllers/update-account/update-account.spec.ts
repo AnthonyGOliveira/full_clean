54783e2687238cf88b2373fd6fe53ab8bd929792
AnthonyGOliveira <anthony.glivers@gmail.com>
2024-11-30 10:21:17 -0300

    test: should call validation when updateAcountController is called

--- src/presentation/controllers/update-account/update-account.spec.ts
+++ src/presentation/controllers/update-account/update-account.spec.ts
@@ -1,14 +1,26 @@
-import { serverError } from "../../helpers/http-helpers";
+import { Validation } from "../../helpers/validators/validation";
 import { UpdateAccountController } from "./update-account";
 
 type SutType = {
+  validation: Validation;
   sut: UpdateAccountController;
 };
 
+const makeValidationCompositeStub = (): Validation => {
+  class ValidationCompositeStub implements Validation {
+    validate(input: any): Error | null {
+      return null;
+    }
+  }
+  return new ValidationCompositeStub();
+};
+
 const makeSut = (): SutType => {
-  const sut = new UpdateAccountController();
+  const validationStub = makeValidationCompositeStub();
+  const sut = new UpdateAccountController(validationStub);
   return {
     sut,
+    validation: validationStub,
   };
 };
 
@@ -31,5 +43,21 @@ describe("UpdateAccountController", () => {
     expect(httpResponse.body.message).toEqual("Internal Server Error");
     expect(httpResponse.body.stack).toBeTruthy();
     expect(httpResponse.statusCode).toBe(500);
+  });
+  test("should call validation when updateAcountController is called", async () => {
+    const { sut, validation } = makeSut();
+    const httpRequest = {
+      body: {
+        role: "any_role",
+        name: "any_name",
+        email: "any_email",
+        old_password: "old_password",
+        password: "any_password",
+        confirmationPassword: "any_password",
+      },
+    };
+    const validationSpy = jest.spyOn(validation, "validate");
+    await sut.handle(httpRequest);
+    expect(validationSpy).toHaveBeenCalledWith(httpRequest.body);
   });
 });
