385263247c32c2cd71bd110eddc3543c7d0aa98f
AnthonyGOliveira <anthony.glivers@gmail.com>
2024-11-30 10:26:00 -0300

    feat: should return 400 if validation return an error

--- src/presentation/controllers/update-account/update-account.ts
+++ src/presentation/controllers/update-account/update-account.ts
@@ -1,4 +1,4 @@
-import { serverError } from "../../helpers/http-helpers";
+import { badRequest, serverError } from "../../helpers/http-helpers";
 import { Validation } from "../../helpers/validators/validation";
 import { Controller } from "../../protocols/controller";
 import { HttpRequest, HttpResponse } from "../../protocols/http";
@@ -7,7 +7,10 @@ export class UpdateAccountController implements Controller {
   constructor(private readonly validation: Validation) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      this.validation.validate(httpRequest.body);
+      const validationError = this.validation.validate(httpRequest.body);
+      if (validationError) {
+        return badRequest(validationError);
+      }
       throw new Error();
     } catch (error) {
       return serverError(error);
