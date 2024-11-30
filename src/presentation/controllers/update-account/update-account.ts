2250eba4cd759cf36978bb78024a4d487a6779ab
AnthonyGOliveira <anthony.glivers@gmail.com>
2024-11-30 10:21:07 -0300

    feat: should call validation when updateAcountController is called

--- src/presentation/controllers/update-account/update-account.ts
+++ src/presentation/controllers/update-account/update-account.ts
@@ -1,10 +1,13 @@
 import { serverError } from "../../helpers/http-helpers";
+import { Validation } from "../../helpers/validators/validation";
 import { Controller } from "../../protocols/controller";
 import { HttpRequest, HttpResponse } from "../../protocols/http";
 
 export class UpdateAccountController implements Controller {
+  constructor(private readonly validation: Validation) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      this.validation.validate(httpRequest.body);
       throw new Error();
     } catch (error) {
       return serverError(error);
