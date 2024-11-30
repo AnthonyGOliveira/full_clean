d7574ed38f372dfcda0378b21332cd9198e01837
AnthonyGOliveira <anthony.glivers@gmail.com>
2024-11-30 12:46:39 -0300

    test: should DbAuthenticationUseCase call PasswordValidator

--- src/data/usecases/update-account/update-account.spec.ts
+++ src/data/usecases/update-account/update-account.spec.ts
@@ -1,10 +1,12 @@
 import { FindAcountModel } from "../../models/find-account-model";
 import { FindAccountByEmailRepository } from "../../protocols/find-account-repository";
+import { PasswordValidator } from "../../protocols/password-validator";
 import { DbUpdateAcountUseCase } from "./update-account";
 
 interface TypeSut {
   sut: DbUpdateAcountUseCase;
   findAccountRepository: FindAccountByEmailRepository;
+  passwordValidator: PasswordValidator;
 }
 
 const makeFindAccountByEmailRepositoryStub =
@@ -27,17 +29,35 @@ const makeFindAccountByEmailRepositoryStub =
     return new FindAccountByEmailRepositoryStub();
   };
 
+const makePasswordValidatorStub = (): PasswordValidator => {
+  class PasswordValidatorStub implements PasswordValidator {
+    async compare(password: string, hash: string): Promise<boolean> {
+      return new Promise((resolve) => resolve(true));
+    }
+  }
+
+  return new PasswordValidatorStub();
+};
+
 const makeSut = (): TypeSut => {
   const findAccountRepository = makeFindAccountByEmailRepositoryStub();
-  const sut = new DbUpdateAcountUseCase(findAccountRepository);
+  const passwordValidatorStub = makePasswordValidatorStub();
+  const sut = new DbUpdateAcountUseCase(
+    findAccountRepository,
+    passwordValidatorStub
+  );
 
   return {
     sut,
     findAccountRepository,
+    passwordValidator: passwordValidatorStub,
   };
 };
 
 describe("DbUpdateAcountUseCase", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
   const updateAccount = {
     id: "any_id",
     role: "any_role",
@@ -56,5 +76,32 @@ describe("DbUpdateAcountUseCase", () => {
     const spyRepository = jest.spyOn(findAccountRepository, "find");
     await sut.execute(updateAccount);
     expect(spyRepository).toHaveBeenCalledWith(updateAccount.email);
+  });
+  test("should DbUpdateAcountUseCase return null if not find account", async () => {
+    const { sut, findAccountRepository } = makeSut();
+    jest.spyOn(findAccountRepository, "find").mockResolvedValueOnce(null);
+    const result = await sut.execute(updateAccount);
+    expect(result).toEqual(null);
+  });
+  test("should DbAuthenticationUseCase call PasswordValidator", async () => {
+    const { sut, passwordValidator } = makeSut();
+    const spyRepository = jest.spyOn(passwordValidator, "compare");
+    const expectHash = "any_hash";
+    await sut.execute(updateAccount);
+    expect(spyRepository).toHaveBeenCalledWith(
+      updateAccount.oldPassword,
+      expectHash
+    );
+  });
+  test("should DbAuthenticationUseCase not call PasswordValidator", async () => {
+    const { sut, passwordValidator } = makeSut();
+    const spyRepository = jest.spyOn(passwordValidator, "compare");
+    await sut.execute({
+      ...updateAccount,
+      oldPassword: undefined,
+      password: undefined,
+      confirmationPassword: undefined,
+    });
+    expect(spyRepository).not.toHaveBeenCalled();
   });
 });
